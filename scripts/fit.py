e670a045834ee695ee900a0b5736f7f5e80bd050
alexnikzotov <miccormicco-risen@yandex.ru>
2025-07-01 11:30:54 +0000

    успешный запуск dvc из второй ветки

--- scripts/fit.py
+++ scripts/fit.py
@@ -6,6 +6,7 @@ from sklearn.pipeline import Pipeline
 from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
@@ -30,11 +31,13 @@ def fit_model():
 
     preprocessor = ColumnTransformer([
             ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            #('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())],
     remainder='drop', verbose_feature_names_out=False)
 
-    model = CatBoostClassifier(auto_class_weights='Balanced')
+    #model = CatBoostClassifier(auto_class_weights='Balanced')
+    model = LogisticRegression(C=1, penalty='l2')
     pipeline = Pipeline([ ('preprocessor', preprocessor),('model', model) ])
 
     data['target'] = [random.choice([0, 1]) for _ in range(len(data))] # Добавлено здесь
